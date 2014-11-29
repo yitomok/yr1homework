8943b9fb72c612e146993ceb7d5b1da25e31c0d3
Yito Mok <yitomok@yahoo.com.hk>
2014-11-30 00:56:46 +0100

    non-recursive, Warnsdorff's rule version of Task 11

--- task11.c
+++ task11.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 #define YES "YES"
 #define NO "NO"
@@ -9,7 +8,6 @@
 int move_x[] = { -1, -2, -2, -1, 1, 2, 2, 1 };
 int move_y[] = { -2, -1, 1, 2, 2, 1, -1, -2 };
 int size = sizeof(move_x) / sizeof(int);
-int row[size];
 
 int ** alloc_board(int m, int n) {
 	int i;
@@ -34,23 +32,58 @@ int ** alloc_board(int m, int n) {
 	return board;
 }
 
-void free_board(int **board, int n) {
+void free_board(int **board, int m) {
 	int i;
 
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < m; i++) {
 		free(*(board + i));
 	}
 	free(board);
 }
 
+int kth_smallest(int *a, int size, int k) {
+	int i;
+	int tmp;
+	int swapped = 0;
+	int c[size];
+	int b[size];
+
+	for (i = 0; i < size; i++) {
+		c[i] = a[i];
+		b[i] = i;
+	}
+
+	if (k < 1) {
+		k = 1;
+	}
+	if (k > size) {
+		k = size;
+	}
+
+	while (swapped++ < k) {
+		for (i = size - 1; i > 0; i--) {
+			if (c[i - 1] > c[i]) {
+				tmp = c[i];
+				c[i] = c[i - 1];
+				c[i - 1] = tmp;
+
+				tmp = b[i];
+				b[i] = b[i - 1];
+				b[i - 1] = tmp;
+			}
+		}
+	}
+
+	return b[k - 1];
+}
+
 int move(int **board, int m, int n, int sx, int sy) {
 	int i, j, k;
 	int next_x, next_y;
-	int stack_x[m * n], stack_y[m * n], stack_dir[m * n], stack_move[m * n][size], stack_smallest[m * n];
+	int stack_x[m * n], stack_y[m * n], stack_dir[m * n], stack_move[m * n][size];
 
 	for (i = 0; i < m * n; i++) {
 		stack_dir[i] = 0;
-		stack_smallest = INT_MAX;
 	}
 
 	stack_x[0] = sx;
@@ -58,12 +91,12 @@ int move(int **board, int m, int n, int sx, int sy) {
 
 	i = 1;
 
-	while (i < m * n && stack_dir[0] < size) {
+	while (i < m * n && stack_dir[i - 1] < size) {
 		for (j = 0; j < size; j++) {
 			stack_move[i - 1][j] = 0;
 		}
 
-		// Warnsdorff's rule
+		// Warnsdorff's rule preparation
 		for (j = 0; j < size; j++) {
 			next_x = stack_x[i - 1] + move_x[j];
 			next_y = stack_y[i - 1] + move_y[j];
@@ -85,35 +118,28 @@ int move(int **board, int m, int n, int sx, int sy) {
 			}
 		}
 
-		stack_dir[i - 1]++;
-		for (j = 0; j < size; j++) {
-			if (stack_move[i - 1] >= 0 && stack_move[i - 1] < stack_smallest) {
-				stack_smallest[i - 1] = j;
-			}
-		}
+		j = stack_dir[i - 1]++;
 
-		//j = stack_dir[i - 1]++;
+		// Apply Warnsdorff's rule, and find a possible tour
+		while (j < size) {
+			j = kth_smallest(stack_move[i - 1], size, stack_dir[i - 1]);
 
-		// Go through each valid directions for a possible tour
-		/*while (j < size) {
 			if (stack_move[i - 1][j] >= 0) {
 				stack_x[i] = stack_x[i - 1] + move_x[j];
 				stack_y[i] = stack_y[i - 1] + move_y[j];
 				board[stack_x[i]][stack_y[i]] = i + 1;
 
+				j = stack_dir[i - 1] - 1;
+
 				break;
 			}
 
 			j = stack_dir[i - 1]++;
-		}*/
+		}
 
-		// Check backtracking criteria
+		// Check valid movement criteria
 		if (j < size) {
 			i++;
-		} else if (i > 1) {
-			board[stack_x[i - 1]][stack_y[i - 1]] = 0;
-			stack_dir[i - 1] = 0;
-			i--;
 		}
 	}
 
@@ -136,7 +162,7 @@ int main() {
 
 	printf(move(board, m, n, sx - 1, sy - 1) ? YES : NO);
 
-	free_board(board, n);
+	free_board(board, m);
 
 	return 0;
 }
